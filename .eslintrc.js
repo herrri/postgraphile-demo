93cd5e1c85b79ced8d70ec0b743a49ac61407fae
Henri Pajala <henripajala@gmail.com>
2022-02-21 22:11:27 +0200

    Prettier plugin for ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,7 @@ module.exports = {
     browser: false,
     es2021: true,
   },
-  extends: [
-    "airbnb-base",
-  ],
+  extends: ["airbnb-base"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
@@ -14,7 +12,5 @@ module.exports = {
     quotes: [2, "double"],
     "no-console": "off",
   },
-  plugins: [
-    "json",
-  ],
+  plugins: ["json", "prettier"],
 };
